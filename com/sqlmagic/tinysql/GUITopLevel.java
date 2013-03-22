e9d76e906054552a0e773045f5132e0d9623416c
Masar Sakr <masarsakr@gmail.com>
2013-03-22 17:57:41 -0400

    Some updates to the top level gui

--- com/sqlmagic/tinysql/GUITopLevel.java
+++ com/sqlmagic/tinysql/GUITopLevel.java
@@ -12,10 +12,21 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JToolBar;
+import javax.swing.JMenuItem;
+import javax.swing.JSplitPane;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JList;
 
 public class GUITopLevel extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -54,24 +65,126 @@ public class GUITopLevel extends JFrame {
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
 		
+		JMenuItem mntmCreateTable = new JMenuItem("Create Table");
+		mnFile.add(mntmCreateTable);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mnFile.add(mntmExit);
+		
 		JMenu mnHelp = new JMenu("Help");
 		menuBar.add(mnHelp);
+		
+		JMenuItem mntmCommands = new JMenuItem("Commands");
+		mnHelp.add(mntmCommands);
+		
+		JMenuItem mntmLimitations = new JMenuItem("Limitations");
+		mnHelp.add(mntmLimitations);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("New Features");
+		mnHelp.add(mntmNewMenuItem);
+		
+		JMenuItem mntmAbout = new JMenuItem("About");
+		mnHelp.add(mntmAbout);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		JToolBar toolBar = new JToolBar();
+		JPanel panel = new JPanel();
+		
+		JPanel panel_1 = new JPanel();
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addComponent(toolBar, GroupLayout.DEFAULT_SIZE, 872, Short.MAX_VALUE)
+				.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 872, Short.MAX_VALUE)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(tabbedPane, GroupLayout.DEFAULT_SIZE, 669, Short.MAX_VALUE))
 		);
 		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addComponent(toolBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(495, Short.MAX_VALUE))
+					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 44, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+						.addComponent(tabbedPane)
+						.addComponent(panel, GroupLayout.DEFAULT_SIZE, 449, Short.MAX_VALUE))
+					.addContainerGap())
 		);
+		
+		JLabel lblTablesInDirectory = new JLabel("0 Tables in Directory");
+		lblTablesInDirectory.setHorizontalAlignment(SwingConstants.LEFT);
+		lblTablesInDirectory.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		JList list = new JList();
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+						.addComponent(list, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 168, Short.MAX_VALUE)
+						.addComponent(lblTablesInDirectory, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 168, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addComponent(lblTablesInDirectory)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(list, GroupLayout.DEFAULT_SIZE, 416, Short.MAX_VALUE))
+		);
+		panel.setLayout(gl_panel);
+		
+		JLabel lblDirectory = new JLabel("Directory");
+		lblDirectory.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		JButton btnBrowse = new JButton("Browse");
+		btnBrowse.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		JLabel lblStatus = new JLabel("Status:");
+		lblStatus.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		JLabel lblstatusHere = new JLabel("*general status here*");
+		
+		JButton btnGo = new JButton("Go");
+		btnGo.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
+		gl_panel_1.setHorizontalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblDirectory)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(textField, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(btnBrowse)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(btnGo)
+					.addPreferredGap(ComponentPlacement.RELATED, 71, Short.MAX_VALUE)
+					.addComponent(lblStatus)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblstatusHere, GroupLayout.PREFERRED_SIZE, 272, GroupLayout.PREFERRED_SIZE))
+		);
+		gl_panel_1.setVerticalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblDirectory)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, 22, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblstatusHere)
+						.addComponent(lblStatus)
+						.addComponent(btnBrowse)
+						.addComponent(btnGo))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		panel_1.setLayout(gl_panel_1);
 		contentPane.setLayout(gl_contentPane);
 	}
 }
