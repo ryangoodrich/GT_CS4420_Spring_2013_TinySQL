c49027882cc5a36bf887fb5e3a617a0aa3f4cd9f
Herman Wu <zwu36@gatech.edu>
2013-04-22 19:28:05 -0400

    gui change

--- com/sqlmagic/tinysql/GUIExecuteSQL.java
+++ com/sqlmagic/tinysql/GUIExecuteSQL.java
@@ -57,43 +57,43 @@ public class GUIExecuteSQL extends JPanel {
 		inputLabel.setBounds(15, 10, 70, 30);
 		this.add(inputLabel);
 		outputLabel = new JLabel("Result");
-		outputLabel.setBounds(15, 110, 50, 20);
+		outputLabel.setBounds(20, 100, 50, 20);
 		this.add(outputLabel);
 		
 		
 		//Text Area
 	    inputArea = new JTextArea();
 		inputArea.setLineWrap(true);
-		inputArea.setBounds(15, 35, 630, 50);
+		inputArea.setBounds(15, 35, 630, 50);	
 		this.add(inputArea);
-	    outputArea = new JTextArea();
-	    outputArea.setEditable(false);
-	    scrollPane = new JScrollPane(outputArea);
-	    scrollPane.setBounds(15, 130, 630, 100);
-		this.add(scrollPane);
+	    //outputArea = new JTextArea();
+	    //outputArea.setEditable(false);
+	    //scrollPane = new JScrollPane(outputArea);
+	    //scrollPane.setBounds(15, 130, 630, 50);
+		//this.add(scrollPane);
 		//JTable
 		resultTable = new JTable(new DefaultTableModel(data, columns));
 		resultTable.setPreferredScrollableViewportSize(new Dimension(450,63));
 		resultTable.setFillsViewportHeight(true);
 		tableScrollPane = new JScrollPane(resultTable); 
-		tableScrollPane.setBounds(15, 240, 630, 160);
+		tableScrollPane.setBounds(15, 120, 630, 300);
 		this.add(tableScrollPane);
 		
 		//Buttons
 		selectButton = new JButton("Select");
-		selectButton.setBounds(250,10,70,25);
+		selectButton.setBounds(290,10,70,25);
 		this.add(selectButton);
 		insertButton = new JButton("Insert");
-		insertButton.setBounds(320,10,70,25);
+		insertButton.setBounds(360,10,70,25);
 		this.add(insertButton);
 		updateButton = new JButton("Update");
-		updateButton.setBounds(390,10,70,25);
+		updateButton.setBounds(430,10,70,25);
 		this.add(updateButton);
 		deleteButton = new JButton("Delete");
-		deleteButton.setBounds(460,10,70,25);
+		deleteButton.setBounds(500,10,70,25);
 		this.add(deleteButton);
 		executeButton = new JButton("Run SQL");
-		executeButton.setBounds(450,90,100,30);
+		executeButton.setBounds(500,88,100,30);
 		this.add(executeButton);
 		
 		
@@ -136,7 +136,6 @@ public class GUIExecuteSQL extends JPanel {
 					
 				}
 						
-		
 					try{
 					execute(inputStream);
 					} catch (SQLException e) {
@@ -201,48 +200,17 @@ public class GUIExecuteSQL extends JPanel {
             
             //setup table
             ResultSet table_rs = stmt.executeQuery(inputQuery);
-            getTableRows(table_rs);
-            
-            meta = display_rs.getMetaData();
-            //setResultTable(meta);
-            //The actual number of columns retrieved has to be checked
-
-            rsColCount = meta.getColumnCount();
-            lineOut = new StringBuffer(100);
-            int[] columnWidths = new int[rsColCount];
-            int[] columnScales = new int[rsColCount];
-            int[] columnPrecisions = new int[rsColCount];
-            int[] columnTypes = new int[rsColCount];
-            String[] columnNames = new String[rsColCount];
-            for ( i = 0; i < rsColCount; i++ )
-            {
-               columnNames[i] = meta.getColumnName(i + 1);
-               if (columnNames[i].indexOf(CACHE_DIVISOR) > 0)
-            	   columnNames[i] = columnNames[i].substring(columnNames[i].indexOf(CACHE_DIVISOR) + 1);
-               columnWidths[i] = meta.getColumnDisplaySize(i + 1);
-               columnTypes[i] = meta.getColumnType(i + 1);
-               columnScales[i] = meta.getScale(i + 1);
-               columnPrecisions[i] = meta.getPrecision(i + 1);
-               if ( columnNames[i].length() > columnWidths[i] )
-                  columnWidths[i] = columnNames[i].length(); 
-               lineOut.append(padString(columnNames[i],columnWidths[i]) + " ");
-               outputArea.setText(lineOut.toString());
-            }
-            //if ( tinySQLGlobals.DEBUG )
-            
-             
-           displayResults(display_rs);
-           
+            displayResults(table_rs);
          }
 		 else if (inputQuery.toUpperCase().startsWith("INSERT")| inputQuery.toUpperCase().startsWith("UPDATE")){
 			 stmt.executeUpdate(inputQuery);
-			 outputArea.setText("DONE\n"); 	 
+			 JOptionPane.showMessageDialog(null, "Finish Query\n" + inputQuery);	 
 		 }
 		 
 		
 	 }
 	
-	public void getTableRows(ResultSet display_rs) throws SQLException{
+	public void displayResults(ResultSet display_rs) throws SQLException{
 		meta = display_rs.getMetaData();
         columns = getColumnName(meta);
         resultTable.setModel(new DefaultTableModel(data, columns));
@@ -264,99 +232,6 @@ public class GUIExecuteSQL extends JPanel {
 		  tca.adjustColumns();
 	   };
 	
-	
-	   private static String padString(String inputString, int padLength)
-	   {
-	      String outputString;
-	      String blanks = "                                        ";
-	      if ( inputString == null )
-	         outputString = blanks + blanks + blanks;
-	      else
-	         outputString = inputString;
-	      if ( outputString.length() > padLength )
-	         return outputString.substring(0,padLength);
-	      else
-	         outputString = outputString + blanks + blanks + blanks;
-	         return outputString.substring(0,padLength);
-	   }
-	   
-	   public int displayResults(ResultSet rs) throws java.sql.SQLException
-	   {
-	      if (rs == null)
-	      {
-	         outputArea.setText("ERROR in displayResult(): No data in ResultSet");
-	         return 0;
-	      }
-	      java.sql.Date testDate;
-	      int numCols = 0,nameLength;
-	      ResultSetMetaData meta = rs.getMetaData();
-	      int cols = meta.getColumnCount();
-	      int[] width = new int[cols];
-	      String dashes = "=============================================";
-	/*
-	 *    Display column headers
-	 */
-	      boolean first=true;
-	      StringBuffer head = new StringBuffer();
-	      StringBuffer line = new StringBuffer();
-	/*
-	 *    Fetch each row
-	 */
-	      while (rs.next()) 
-	      {
-	/*
-	 *       Get the column, and see if it matches our expectations
-	 */
-	         String text = new String();
-	         for (int ii=0; ii<cols; ii++)
-	         {
-	            String value = rs.getString(ii+1);
-	            if (first)
-	            {
-	               width[ii] = meta.getColumnDisplaySize(ii+1);
-	               if ( tinySQLGlobals.DEBUG &
-	                    meta.getColumnType(ii+1) == Types.DATE )
-	               {
-	                  testDate = rs.getDate(ii+1);
-	                  outputArea.append("Value " + value + ", Date "
-	                  + testDate.toString());
-	               }
-	               nameLength = meta.getColumnName(ii+1).length();
-	               if ( nameLength > width[ii] ) width[ii] = nameLength;
-	               line.append(padString(dashes+dashes,width[ii]));
-	               line.append(" ");
-	            }
-	            text += padString(value, width[ii]);
-	            text += " ";   // the gap between the columns
-	         }
-	         try
-	         {
-	            if (first) 
-	            {
-	               if ( spoolFileWriter != null ) 
-	               {
-	                  spoolFileWriter.write(head.toString() + newLine);
-	                  spoolFileWriter.write(head.toString() + newLine);
-	               } else {
-	                 outputArea.append(head.toString()+ "\n");
-	                 outputArea.append(line.toString()+ "\n");
-	               }
-	               first = false;
-	            }
-	            if ( spoolFileWriter != null ) 
-	               spoolFileWriter.write(text + newLine);
-	            else
-	            	outputArea.append(text + "\n");
-	            numCols++;
-	         } catch ( Exception writeEx ) {
-	        	 outputArea.append("Exception writing to spool file " 
-	            + writeEx.getMessage());
-	         }
-	      }
-	      return numCols;
-	   }
-	   
-
 	   
 	   
 	   
